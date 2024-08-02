eba99d6b335c41eb49ff286285c7148fd78ebd27
IshaSaxena-DAX <Isha.Saxena@data-axle.com>
2024-08-02 23:17:49 +0530

    adding migration

--- db/migrate/20240802173449_add_timestamps_to_migration.rb
+++ db/migrate/20240802173449_add_timestamps_to_migration.rb
@@ -1,6 +1,6 @@
 class AddTimestampsToMigration < ActiveRecord::Migration[7.1]
   def change
-    add_column :articles, :created, :datetime
+    add_column :articles, :created, :datetime #articles= table name , created = attribute, datetime= datatype
     add_column :articles, :updated, :datetime
   end
 end
